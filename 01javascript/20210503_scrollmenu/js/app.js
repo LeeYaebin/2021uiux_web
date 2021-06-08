2b171f90dba701186b494821f156689f078a804d
qls2920 <qls2920@naver.com>
2021-06-08 14:39:22 +0900

    .

---
+++ 01javascript/20210503_scrollmenu/js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+  //1menu
+  const oneTop = $("#v01s").offset().top;
+  const oneTwo = $("#v02s").offset().top;
+  const oneThree = $("#v03s").offset().top;
+
+  // $(".menu > a").eq(0).on("click",function(){});
+  // $(".menu>a").eq(0).click(function(){
+  //   $("html,body").animate({scrollTop:oneTop})
+  // });
+  // $(".menu>a").eq(1).click(function(){
+  //   $("html,body").animate({scrollTop:oneTwo})
+  // });
+  // $(".menu>a").eq(2).click(function(){
+  //   $("html,body").animate({scrollTop:oneThree})
+  // });
+
+  //for
+  // for(let i=0; i<$(".menu>a").length;i++){
+  //   $(".menu>a").eq(i).click(function(){
+  //     $("html,body").animate({scrollTop:$(".scene").eq(i).offset().top})
+  //   });
+  // }
+
+
+  //$(this).index()
+  // $(".menu>a").click(function(){
+  //   let i = $(this).index()
+  //   $("html,body").animate({scrollTop:$(".scene").eq(i).offset().top})
+  // });
+
+  // $(".menu>a").click(function(){
+  //   let i = $(this).index();
+  //   const st = $(".scene").eq(i).offset().top;
+  //   $("html,body").animate({scrollTop:st});
+  // });
+
+  $(".menu>a").click(function(){
+    let target = $(this.hash);
+    console.log(target);
+    const st = target.offset().top;
+    $("html,body").animate({scrollTop:st})
+  });
+
+  // $(window).scroll(function(){
+  //   let scrollY = $(window).scrollTop();
+  //   if(scrollY >= $(".scene").eq(0).offset().top){
+  //     $(".menu>a").removeClass("active").eq(0).addClass("active")
+  //   }
+  //   if(scrollY >= $(".scene").eq(1).offset().top){
+  //     $(".menu>a").removeClass("active").eq(1).addClass("active")
+  //   }
+  //   if(scrollY >= $(".scene").eq(2).offset().top){
+  //     $(".menu>a").removeClass("active").eq(2).addClass("active")
+  //   }
+  //});
+
+  // for(let i=0; i<$(".scene").length; i++){
+  //   if(scrollY >= $(".scene").eq(i).offset().top){
+  //     $(".menu>a").removeClass("active").eq(i).addClass("active");
+  //   }
+  // }
+
+  // for(let i=0; i<$(".scene").length; i++){
+  //   if(scrollY >= $(".scene").eq(i).offset().top){
+  //     $(".menu>a").removeClass("active").eq(i).addClass("active");
+  //   }
+  // }
+
+  $(".scene").each(function(index){
+    // if(scrollY >= $(this).offset().top){}
+    if(scrollY >= $(this).offset().top){
+      $(".menu>a").removeClass("active").eq(index).addClass("active");
+    }
+  });
+
+}); //jquery  ready
