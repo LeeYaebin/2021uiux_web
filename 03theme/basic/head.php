029da48144bfc72a4034d4319d8b0b30e1e03463
qls2920 <qls2920@naver.com>
2021-06-28 15:32:03 +0900

    .

--- 03theme/basic/head.php
+++ 03theme/basic/head.php
@@ -29,23 +29,20 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
     <div id="hd_wrapper">
 
         <div id="logo">
-            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
+            <a href="<?php echo G5_URL ?>"><img src="<?php echo G5_THEME_IMG_URL ?>/logo.png" alt="<?php echo $config['cf_title']; ?>"></a>
         </div>
 
-        <div class="hd_sch_wr">
 
-
-            <?php echo popular('theme/basic'); // 인기검색어, 테마의 스킨을 사용하려면 스킨을 theme/basic 과 같이 지정  ?>
-        </div>
         <ul class="hd_login">
             <?php if ($is_member) {  ?>
-            <li><a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a></li>
-            <?php if ($is_admin) {  ?>
-            <li class="tnb_admin"><a href="<?php echo correct_goto_url(G5_ADMIN_URL); ?>">관리자</a></li>
-            <?php }  ?>
+              <li><a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=<?php echo G5_BBS_URL ?>/register_form.php">정보수정</a></li>
+              <li><a href="<?php echo G5_BBS_URL ?>/logout.php">로그아웃</a></li>
+              <?php if ($is_admin) {  ?>
+              <li class="tnb_admin"><a href="<?php echo correct_goto_url(G5_ADMIN_URL); ?>">관리자</a></li>
+              <?php }  ?>
             <?php } else {  ?>
-            <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
-            <li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li>
+              <li><a href="<?php echo G5_BBS_URL ?>/register.php">회원가입</a></li>
+              <li><a href="<?php echo G5_BBS_URL ?>/login.php">로그인</a></li>
             <?php }  ?>
 
         </ul>
@@ -57,8 +54,8 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
             <ul id="gnb_1dul">
                 <li class="gnb_1dli gnb_mnal"><button type="button" class="gnb_menu_btn" title="전체메뉴"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only">전체메뉴열기</span></button></li>
                 <?php
-				$menu_datas = get_menu_db(0, true);
-				$gnb_zindex = 999; // gnb_1dli z-index 값 설정용
+            $menu_datas = get_menu_db(0, true);
+            $gnb_zindex = 999; // gnb_1dli z-index 값 설정용
                 $i = 0;
                 foreach( $menu_datas as $row ){
                     if( empty($row) ) continue;
@@ -148,33 +145,68 @@ include_once(G5_LIB_PATH.'/popular.lib.php');
 
 <hr>
 
-<ul class="slide">
-    <li><img src="<? echo G5_THEME_URL ?>/img/pc01.jpg" alt=""></li>
-    <li><img src="<? echo G5_THEME_URL ?>/img/pc02.jpg" alt=""></li>
-    <li><img src="<? echo G5_THEME_URL ?>/img/pc03.jpg" alt=""></li>
-</ul>
-<script>
-$(".slide").bxSlider({
-  auto:true,
-  controls:false,
-  pager:false
-});
-</script>
+<?php if(defined('_INDEX_')) { ?>
+<div class="slider">
+  <img src="<? echo G5_THEME_IMG_URL ?>/pc01.jpg" alt="">
+  <img src="<? echo G5_THEME_IMG_URL ?>/pc02.jpg" alt="">
+  <img src="<? echo G5_THEME_IMG_URL ?>/pc03.jpg" alt="">
+</div>
 <style>
 .bx-wrapper {
-    -moz-box-shadow: none;
-    -webkit-box-shadow: none;
-    box-shadow: none;
-    border:0 none;
-    width:1200px;
-    margin:0 auto;
+  -moz-box-shadow: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
+  border: 0 none;
 }
-
 </style>
+
+<script>
+  $(".slider").bxSlider({
+    pager:false,
+    auto:true
+  });
+</script>
+<?php } ?>
+
+<? if(!defined('_INDEX_')){?>
+  <div class="subVisual">
+    <div class="subImg" id="page_title">
+      <div class="title">
+        <h2 class="loc1D"></h2>
+        <div class="txt">
+          안녕하세요.
+        </div>
+      </div>
+    </div>
+  </div>
+  <script>
+  window.onload = function(){
+    const menuDep = $(".loc1D").html(); //get
+    console.log("현재위치" + menuDep);
+
+    if(menuDep == "회사소개" ){
+      $(".subVisual .txt").html("저희 홈페이지를 찾아주셔서 감사합니다.")
+    }
+    if(menuDep == "제품소개"){
+      $(".subVisual .txt").html("제품소개입니다.")
+    }
+    if(menuDep == "커뮤니티"){
+      $(".subvisual .txt").html("자유롭게 커뮤니티 하실 수 있는 곳입니다.")
+    }
+    };
+  </script>
+<? }?>
+
+
 
 <!-- 콘텐츠 시작 { -->
 <div id="wrapper">
     <div id="container_wr">
 
-    <div id="container">
-        <?php if (!defined("_INDEX_")) { ?><h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }
+      <div id="container" <?php if(defined('_INDEX_')){?>style="width:1200px"<?}?>>
+              <?php if (!defined("_INDEX_")) { ?>
+
+              <!-- location   -->
+              <div>H > <span class="loc1D"></span> > <?php echo get_text($g5['title']); ?></div>
+
+              <h2 id="container_title"><span title="<?php echo get_text($g5['title']); ?>"><?php echo get_head_title($g5['title']); ?></span></h2><?php }?>
